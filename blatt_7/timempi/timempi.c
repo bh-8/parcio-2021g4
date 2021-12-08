aa0499b0f4a00662d7f4e60bfc3e184d752f3ff4
birne420 <73957626+birne420@users.noreply.github.com>
2021-12-08 11:35:37 +0100

    Unix EOL

--- blatt_7/timempi/timempi.c
+++ blatt_7/timempi/timempi.c
@@ -6,23 +6,23 @@
 #include <sys/time.h>
 
 int main() {
+    char h_string[256];
+	
     struct timeval t_value;
     time_t t_seconds;
     int t_micros;
     struct tm *t_formatting;
     char t_string[64];
-
-    char h_string[256];
+	
+    gethostname(h_string, sizeof(h_string));
     
     gettimeofday(&t_value, NULL);
     t_seconds = t_value.tv_sec;
     t_micros = t_value.tv_usec;
-
+	
     t_formatting = localtime(&t_seconds);
     strftime(t_string, sizeof(t_string), "%Y-%m-%d_%H:%M:%S_", t_formatting);
-
-    gethostname(h_string, sizeof(h_string));
-
-    printf("%s%d: %s\n", t_string, t_micros, h_string);
+	
+    printf("%s: %s%d\n", h_string, t_string, t_micros);
     return 0;
 }
