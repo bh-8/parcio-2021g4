e1a6fd54db17ff2eabae6014c123f99373df082b
Philipp David <pd@3b.pm>
2021-12-11 18:13:54 +0100

    Teil von Blatt 7 Aufgabe 2

--- blatt_7/circle/circle.c
+++ blatt_7/circle/circle.c
@@ -1,14 +1,16 @@
+#define _MPI_BUFFER_SIZE 256
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
 
 int*
-init(int N)
+init(int N, int maxsize, int rank)
 {
-	// TODO
-	int* buf = malloc(sizeof(int) * N);
+	int* buf = malloc(sizeof(int) * maxsize);
 
-	srand(time(NULL));
+	srand(time(NULL) ^ (long)rank);
 
 	for (int i = 0; i < N; i++)
 	{
@@ -19,48 +21,97 @@ init(int N)
 	return buf;
 }
 
-int*
-circle(int* buf)
+int
+circle(int* buf, int rank, int size, int N)
 {
 	// TODO
-	return buf;
+	int follower = (rank + 1) % size;
+	int previous = (rank - 1) < 0 ? size - 1 : rank - 1;
+
+	// Return the last used N
+	return N;
+}
+
+void
+print_arr(int *buf, int rank, int size, int N, int before)
+{
+	char str[_MPI_BUFFER_SIZE];
+
+	if (rank == 0)
+	{
+		if (before == 0)
+			printf("\nAFTER\n");
+		else
+			printf("\nBEFORE\n");
+
+		for (int i = 0; i < N; i++)
+		{
+			printf("rank %d: %d\n", rank, buf[i]);
+		}
+		for (int i = 1; i < size; ++i)
+		{
+			int other_N;
+			MPI_Recv(&other_N, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			for (int j = 0; j < other_N; ++j)
+			{
+				MPI_Recv(&str, _MPI_BUFFER_SIZE, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				printf(str);
+			}
+		}
+	}
+	else
+	{
+		MPI_Send(&N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		for (int i = 0; i < N; ++i)
+		{
+			snprintf(str, _MPI_BUFFER_SIZE, "rank %d: %d\n", rank, buf[i]);
+			MPI_Send(&str, _MPI_BUFFER_SIZE, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+		}
+	}
 }
 
 int
 main(int argc, char** argv)
 {
+	MPI_Init(&argc, &argv);
+	
 	int  N;
 	int  rank;
+	int  size;
 	int* buf;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	if (argc < 2)
 	{
-		printf("Arguments error!\nPlease specify a buffer size.\n");
+		if (rank == 0)
+			printf("Arguments error!\nPlease specify a buffer size.\n");
 		return EXIT_FAILURE;
 	}
 
 	// Array length
+	// TODO: Proper input validation, maybe
+	// TODO: How to divide the array when there are more processes than its len
 	N   = atoi(argv[1]);
-	buf = init(N);
+	int count = N / size;
+	int remainder = N % size;
+	N = count;
+	int maxsize = remainder == 0 ? N : N + 1;
 
-	// TODO
-	rank = 0;
+	N += rank < remainder ? 1 : 0;
+	
+	buf = init(N, maxsize, rank);
 
-	printf("\nBEFORE\n");
+	print_arr(buf, rank, size, N, 1);
 
-	for (int i = 0; i < N; i++)
-	{
-		printf("rank %d: %d\n", rank, buf[i]);
-	}
+	N = circle(buf, rank, size, N);
 
-	circle(buf);
+	MPI_Barrier(MPI_COMM_WORLD);
 
-	printf("\nAFTER\n");
+	print_arr(buf, rank, size, N, 0);
 
-	for (int j = 0; j < N; j++)
-	{
-		printf("rank %d: %d\n", rank, buf[j]);
-	}
+	MPI_Finalize();
 
 	return EXIT_SUCCESS;
 }
