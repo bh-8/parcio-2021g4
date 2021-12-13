d1b74a5aed81a467aa132393cf6b3fcd84de7a7a
Philipp David <pd@3b.pm>
2021-12-13 10:58:37 +0100

    Spaces to tab

--- blatt_7/circle/circle_alt.c
+++ blatt_7/circle/circle_alt.c
@@ -121,7 +121,7 @@ main(int argc, char** argv)
 	int* buf;
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	if (argc < 2 && rank == 0)
 	{
